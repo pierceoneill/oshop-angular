471d802ea0066244461b767078d0f90de514ce21
Pierce O'Neill <pierceoneillky@gmail.com>
2019-01-11 00:19:58 +0000

    Addition of Auth - login - logut function

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAwYWQuEEa29K9YqPwjxigYm3JqkJxE5FM",
+    authDomain: "oshop-31932.firebaseapp.com",
+    databaseURL: "https://oshop-31932.firebaseio.com",
+    projectId: "oshop-31932",
+    storageBucket: "oshop-31932.appspot.com",
+    messagingSenderId: "579689232102"
+  }
 };
